c73d3b0fab4e454bf98df3193b996fd7a544772f
lenovo <soniahassouna1@gmail.com>
2018-02-27 18:16:42 +0000

    qlq modif

--- app/src/main/java/com/example/aspirev/selki/MainActivity.java
+++ app/src/main/java/com/example/aspirev/selki/MainActivity.java
@@ -1,19 +1,12 @@
 package com.example.aspirev.selki;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
@@ -31,7 +24,6 @@ public class MainActivity extends AppCompatActivity
         FragmentTransaction.commit();
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -53,9 +45,6 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
-
-
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
